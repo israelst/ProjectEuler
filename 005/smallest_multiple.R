5b27b1fb604c35ee9bb336aefac1240644f289aa
Israel Teixeira <israelst@gmail.com>
2014-05-17 22:55:05 -0300

    Problem 005 solved

--- 005/smallest_multiple.R
+++ 005/smallest_multiple.R
@@ -1,15 +1,15 @@
 smallest_multiple <- function(greatest_divisor){
-    is.smallest = F
-    smallest <- 0
     divisors <- seq(greatest_divisor)
-    while(!is.smallest){
-        is.smallest = T
-        smallest <- smallest + 1
-        for(divisor in divisors){
-            is.smallest <- is.smallest && smallest %% divisor == 0
+    factors <- c(1)
+    for(divisor in divisors){
+        for(factor in factors){
+            if(divisor %% factor == 0){
+                divisor <- divisor / factor
+            }
         }
+        factors <- append(factors, divisor)
     }
-    smallest
+    prod(factors)
 }
 
 print(smallest_multiple(1) == 1)
@@ -17,6 +17,8 @@ print(smallest_multiple(2) == 2)
 print(smallest_multiple(3) == 6)
 print(smallest_multiple(4) == 12)
 print(smallest_multiple(5) == 60)
+print(smallest_multiple(6) == 60)
+print(smallest_multiple(7) == 420)
 print(smallest_multiple(10) == 2520)
 print(smallest_multiple(20))
 
